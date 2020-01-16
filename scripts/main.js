c986a5e2c136546458da521926975053ea16a290
viktorevstigneev <viktor_evstigneev14@bk.ru>
2020-01-16 22:08:55 +0300

    go

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function test(){
+  var audio = document.createElement('audio');
+  audio.setAttribute("autoplay","true");
+  audio.innerHTML = "<source src=\"sound/test.mp3\" type=\"audio/mpeg\">";
+  document.body.appendChild(audio);
+
+  customer.addEventListener(`click`, () => {
+    
+  });
